6324a9886589e666e0da71da22b85b9b1a35956f
Akhad <123157414+akhadbakirov1@users.noreply.github.com>
2024-08-17 16:04:18 +0400

    Добавлен новый компонент [название компонента]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import MiniApp from "./miniApp/MiniApp"
+import Ks from "./miniApp/ks";
 function App() {
   console.log();
   return (
     <>
       <MiniApp></MiniApp>
-      
+        <Ks></Ks>
     </>
   )
 }
